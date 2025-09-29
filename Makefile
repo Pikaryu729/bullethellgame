2899441912ad79d6aae2f80473f9e43d2b55e8c0
Ryu Wells <ryushin.wells@gmail.com>
2025-09-29 14:07:35 -0500

    initialized player, game, sdl

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ CFLAGS = -std=c17 -Wall -Wextra -Werror
 
 all:
 	gcc game.c -o game $(CFLAGS) `sdl2-config --cflags --libs`
+
+debug:
+	gcc -g game.c -o game $(CFLAGS) `sdl2-config --cflags --libs` -DDEBUG
--- game.c
+++ game.c
@@ -8,10 +8,13 @@
 
 const int32_t base_max_hp = 100;
 const int32_t base_damage = 5;
+const int32_t bullet_color = 0xFF0000FF;
 
 typedef struct {
   SDL_Window *window;
   SDL_Renderer *renderer;
+  int32_t bg_color;
+  int32_t fg_color;
 } sdl_t;
 
 typedef struct {
@@ -23,7 +26,8 @@ typedef struct {
   int32_t hp;
   int32_t max_hp;
   int32_t damage;
-  pos_t pos;
+  int32_t color;
+  SDL_Rect rect;
 } player_t;
 
 typedef enum {
@@ -52,6 +56,12 @@ bool init_sdl(sdl_t *sdl) {
     SDL_Log("Error creating renderer: %s \n", SDL_GetError());
     return false;
   }
+
+  // initialize background to black
+  sdl->bg_color = 0x000000FF;
+  // foreground color white
+  sdl->fg_color = 0xFFFFFFFF;
+
   return true;
 }
 
@@ -59,26 +69,82 @@ void init_player(player_t *player) {
   player->max_hp = base_max_hp;
   player->hp = player->max_hp;
   player->damage = base_damage;
-  player->pos.x = WINDOW_WIDTH/ 2;
-  player->pos.y = WINDOW_HEIGHT / 2;
+
+  player->color = 0xFFFFFFFF;
+
+  player->rect.x = WINDOW_WIDTH / 2;
+  player->rect.y = WINDOW_HEIGHT / 2;
+  player->rect.h = 25;
+  player->rect.w = 25;
 }
 
-void draw_player(player_t *player) {
+void init_game(game_t *game) { game->state = RUNNING; }
 
+void draw_player(sdl_t *sdl, player_t *player) {
+  // extract player color rgb
+  int8_t color_r = (player->color >> 24) & 0xFF;
+  int8_t color_g = (player->color >> 16) & 0xFF;
+  int8_t color_b = (player->color >> 8) & 0xFF;
+  int8_t color_a = (player->color);
+
+  // set the render color to the color of the player
+  SDL_SetRenderDrawColor(sdl->renderer, color_r, color_g, color_b, color_a);
+  SDL_RenderFillRect(sdl->renderer, &player->rect);
 }
 
-void cleanup() { SDL_Quit(); }
+void update_screen(sdl_t *sdl, player_t *player) {
+  // extract bg color rgb
+  int8_t color_r = (sdl->bg_color >> 24) & 0xFF;
+  int8_t color_g = (sdl->bg_color >> 16) & 0xFF;
+  int8_t color_b = (sdl->bg_color >> 8) & 0xFF;
+  int8_t color_a = (sdl->bg_color);
+
+  SDL_SetRenderDrawColor(sdl->renderer, color_r, color_g, color_b, color_a);
+  SDL_RenderClear(sdl->renderer);
+
+  draw_player(sdl, player);
+
+  SDL_RenderPresent(sdl->renderer);
+}
+
+void handle_input(game_t *game) {
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+    case SDL_QUIT:
+      game->state = QUIT;
+      break;
+    case SDL_KEYDOWN:
+      switch (event.key.keysym.sym) {
+      case SDLK_ESCAPE:
+        game->state = QUIT;
+        break;
+      }
+    }
+  }
+}
+void cleanup(void) { SDL_Quit(); }
 
 int main(void) {
 
+  // initialize sdl subsystems, create window and renderer
   sdl_t sdl;
   if (!init_sdl(&sdl)) {
     cleanup();
     exit(1);
   }
+  // set game state to running
+  game_t game;
+  init_game(&game);
 
+  // set player default values
   player_t player;
   init_player(&player);
+
+  while (game.state != QUIT) {
+    handle_input(&game);
+    update_screen(&sdl, &player);
+  }
 
   cleanup();
 }
